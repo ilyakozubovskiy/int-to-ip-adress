ca7c016bb329e7143b807047c5ef870fd3f87558
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-05-16 11:18:28 +0300

    Add project files.

---
+++ int to ip-adress/Solution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Kata
+{
+    public static string UInt32ToIP(uint ip)
+    {
+        string binary = Convert.ToString(ip, 2);
+        binary = new string('0', 32 - binary.Length) + binary;
+
+        return Convert.ToUInt32(binary[..8], 2) + "." +
+               Convert.ToUInt32(binary[8..16], 2) + "." +
+               Convert.ToUInt32(binary[16..24], 2) + "." +
+               Convert.ToUInt32(binary[24..32], 2);
+
+    }
+}
